43151b4acd04f41ad29197205f92d37a9eaf0a5c
Michal Majewski <60788689+mgxmajewski@users.noreply.github.com>
2026-05-25 23:35:50 +0200

    chore: add .idea

--- Migrations/20260525220342_AddMissingPCComponentsSeed.Designer.cs
+++ Migrations/20260525220342_AddMissingPCComponentsSeed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PJATK_APBD_Cw4_s32103.DAL;
 
@@ -11,9 +12,11 @@ using PJATK_APBD_Cw4_s32103.DAL;
 namespace PJATK_APBD_Cw4_s32103.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260525220342_AddMissingPCComponentsSeed")]
+    partial class AddMissingPCComponentsSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260525220342_AddMissingPCComponentsSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PJATK_APBD_Cw4_s32103.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingPCComponentsSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PCComponents",
+                columns: new[] { "ComponentCode", "PCId", "Amount" },
+                values: new object[,]
+                {
+                    { "CPU0000001", 2, 1 },
+                    { "RAM0000001", 2, 1 },
+                    { "CPU0000001", 3, 1 },
+                    { "GPU0000001", 3, 2 },
+                    { "RAM0000001", 3, 4 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PCComponents",
+                keyColumns: new[] { "ComponentCode", "PCId" },
+                keyValues: new object[] { "CPU0000001", 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PCComponents",
+                keyColumns: new[] { "ComponentCode", "PCId" },
+                keyValues: new object[] { "RAM0000001", 2 });
+
+            migrationBuilder.DeleteData(
+                table: "PCComponents",
+                keyColumns: new[] { "ComponentCode", "PCId" },
+                keyValues: new object[] { "CPU0000001", 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PCComponents",
+                keyColumns: new[] { "ComponentCode", "PCId" },
+                keyValues: new object[] { "GPU0000001", 3 });
+
+            migrationBuilder.DeleteData(
+                table: "PCComponents",
+                keyColumns: new[] { "ComponentCode", "PCId" },
+                keyValues: new object[] { "RAM0000001", 3 });
+        }
+    }
+}
